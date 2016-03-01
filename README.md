0f653099d3d46bbe65054efbdd6a89d684c7c8cc
infinitetutts <ryan@tutanota.com>
2016-03-01 14:01:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TS3_startup_script
+# For running 2 TS3 Servers
+# Install location: /etc/init.d/
---
+++ ts3server2_run_files/ts3server2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Start ts3 server in background
+nohup teamspeak3-server_linux-amd64/ts3server_minimal_runscript.sh inifile=ts3server.ini &
+
